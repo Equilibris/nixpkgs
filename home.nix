0489d6ceea59c350f13f17c1ab3fff4af23c0e5d
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-06-19 15:32:22 +0200

    Misc changes

--- home.nix
+++ home.nix
@@ -3,19 +3,21 @@
 {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
+    thefuck
     htop
-    vscode
-    neovim
+    vscode neovim
     discord
     google-chrome
-    git
-    gh
-    clang
-    libcxx
-    libcxxabi 
+    git gh
+    clang libcxx libcxxabi 
+
+    nodejs
+    rustup
 
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
+
+  home.sessionPath = [ "$HOME/.npm-global/bin" ];
 
   # environment.variables.EDITOR = "nvim";
   nixpkgs.overlays = [
@@ -26,10 +28,53 @@
 
   fonts.fontconfig.enable = true;
 
+  gtk = {
+    enable = true;
+
+    theme.name = "Orchis";
+    theme.package = pkgs.orchis-theme;
+  };
+
   programs.git = {
     enable = true;
     userName = "William Sørensen";
     userEmail = "47296141+Equilibris@users.noreply.github.com";
+  };
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      stty werase '^h'
+    '';
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    # plugins = [
+    #   {
+    #     name = "zsh-nix-shell";
+    #     file = "nix-shell.plugin.zsh";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "chisui";
+    #       repo = "zsh-nix-shell";
+    #       rev = "v0.5.0";
+    #       sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+    #     };
+    #   }
+    # ];
+    initExtra = ''
+      npm set prefix ~/.npm-global
+
+      eval "$(~/.config/nixpkgs/tmp/oh-my-posh init zsh)"
+    '';
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
   };
 
   programs.kitty = {
@@ -37,27 +82,44 @@
 
     font.name = "FiraCode Nerd Font";
     extraConfig = ''
-        # background            #c5cdd9
-        # foreground            #2b2d37
-        # cursor                #4C566A
-        # selection_background  #dedede
-        # color0                #212121
-        # color8                #424242
-        # color1                #b7141e
-        # color9                #e83a3f
-        # color2                #457b23
-        # color10               #7aba39
-        # color3                #f5971d
-        # color11               #fee92e
-        # color4                #134eb2
-        # color12               #53a4f3
-        # color5                #550087
-        # color13               #a94dbb
-        # color6                #0e707c
-        # color14               #26bad1
-        # color7                #eeeeee
-        # color15               #d8d8d8
-        # selection_foreground #222221
+      foreground            #D8DEE9
+      background            #2E3440
+      selection_foreground  #000000
+      selection_background  #FFFACD
+      url_color             #0087BD
+      cursor                #81A1C1
+      
+      # black
+      color0   #3B4252
+      color8   #4C566A
+      
+      # red
+      color1   #BF616A
+      color9   #BF616A
+      
+      # green
+      color2   #A3BE8C
+      color10  #A3BE8C
+      
+      # yellow
+      color3   #EBCB8B
+      color11  #EBCB8B
+      
+      # blue
+      color4  #81A1C1
+      color12 #81A1C1
+      
+      # magenta
+      color5   #B48EAD
+      color13  #B48EAD
+      
+      # cyan
+      color6   #88C0D0
+      color14  #8FBCBB
+      
+      # white
+      color7   #E5E9F0
+      color15  #ECEFF4
       '';
 
     settings = { 
@@ -72,7 +134,7 @@
       recursive = true;
     };
     ".local/share/nvim/site/autoload/plug.vim" = {
-      source = ./nvim-prelude/plug.vim;
+      source = ./tmp/plug.vim;
     };
   };
 }
--- install-global.sh
+++ install-global.sh
@@ -1,2 +1,11 @@
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+nix-channel --update
+
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+sudo nix-channel --update
+
 ls -1 ~/.config/nixpkgs/global | while read line; do sudo ln "global/$line" "/etc/nixos/$line"; done
 
