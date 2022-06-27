0895c5a2c3a0cb729c4f90b2b1604534e52f6423
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-06-27 21:07:20 +0200

    Auto install BD plugins

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [ ./term.nix ./lang.nix ];
@@ -59,11 +59,25 @@
         }
       }/plug.vim";
     };
-    ".config/BetterDiscord/themes/nordic.theme.css" = {
+  } // lib.lists.fold (curr: acc: acc // {
+    ".config/BetterDiscord/${curr.name}" = {
       source = pkgs.fetchurl {
-        url = "https://betterdiscord.app/Download?id=33";
-        sha256 = "hazRQ45BSkOZ4YnqDw/xTzcXowBgG762vP8zhNrnEZ4=";
+        url = "http://betterdiscord.app/Download?id=${toString curr.id}";
+        sha256 = if builtins.hasAttr "sha256" curr then curr.sha256 else "";
       };
     };
-  };
+  }) {} [ 
+    {
+      name = "themes/nordic.theme.css"; id = 33; 
+      sha256 = "hazRQ45BSkOZ4YnqDw/xTzcXowBgG762vP8zhNrnEZ4=";
+    }
+    {
+      name = "themes/ClearVision.theme.css"; id = 23;
+      sha256 = "3YgErNGy1Q7tTPrQ1R9U/2b9GY2DdhxjbAF/P2aZXyA=";
+    }
+    {
+      name = "plugins/ShowHiddenChannels.plugin.js"; id = 103;
+      sha256 = "DZmbmc7JFR38o9dHeZLhJkQc/jjkNbUxCj6n6ibXxgs=";
+    }
+  ];
 }
