b3cc434d16be3267d38083e797c1c2b2aab9aa4b
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-06-28 12:49:59 +0200

    Add java and AWS

--- lang.nix
+++ lang.nix
@@ -1,16 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, fetchurl, ... }:
 
 let 
-  java = false;
+  java = true;
   rust = true;
   js   = true;
+
+  docker = true;
+
+  aws = true;
 in 
   {
     home.packages = with pkgs;
          lib.optionals js   [ nodejs ]
       ++ lib.optionals rust [ rustup ]
-      ++ lib.optionals java [ maven openjdk17 kotlin ];
-  }
+      ++ lib.optionals java [ maven jetbrains.jdk /* openjdk */ kotlin jetbrains.idea-community ]
+
+      ++ lib.optionals docker [ pkgs.docker docker-compose ]
+      ++ lib.optionals aws    [ awscli2 ]; }
   # monoidMerge true {} 
   # (monoidMerge rust {}
   # (monoidMerge nodejs {} {}))
