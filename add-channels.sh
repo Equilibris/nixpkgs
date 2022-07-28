5eeaa3375606f566004ad8b38025e015eb53330a
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-07-28 15:04:48 +0200

    Add scripts

---
+++ add-channels.sh
@@ -0,0 +1,7 @@
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+nix-channel --update
+
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+sudo nix-channel --update
--- install-global.sh
+++ install-global.sh
@@ -1,11 +1,21 @@
+DATE=$(date +%F)
+BACKUP="/etc/nixos/backup-$DATE"
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-nix-channel --update
+GLOBALS="/home/williams/.config/nixpkgs/global"
 
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-sudo nix-channel --update
+sudo mkdir $BACKUP
+sudo mv /etc/nixos/*.nix $BACKUP
 
-ls -1 ~/.config/nixpkgs/global | while read line; do sudo ln "global/$line" "/etc/nixos/$line"; done
+ls -1 $GLOBALS | while read line; do sudo ln "$GLOBALS/$line" "/etc/nixos/$line"; done
+
+OPTION=$(echo "$(ls -1 "$GLOBALS/hardware-configuration")
+new" | fzf)
+
+if [ $OPTION = new ]
+then
+    cp "$BACKUP/hardware-configuration.nix" "$GLOBALS/hardware-configuration/hardware-configuration.new.nix"
+    sudo ln "$GLOBALS/hardware-configuration/hardware-configuration.new.nix" "/etc/nixos/hardware-configuration.nix"
+else
+    sudo ln "$GLOBALS/hardware-configuration/$OPTION" "/etc/nixos/hardware-configuration.nix"
+fi
 
