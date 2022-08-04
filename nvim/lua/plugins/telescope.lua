b943a3ba48252f70e3cba4f49f90d5b760f69c6e
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-08-05 00:07:11 +0200

    feat: global telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,16 @@
+local actions = require 'telescope.actions'
+
+local global_mappings = {
+	['<A-j>'] = actions.move_selection_next,
+	['<A-k>'] = actions.move_selection_next,
+}
+
 require('telescope').setup {
-	defaults = { file_ignore_patterns = { 'target' } },
+	defaults = {
+		file_ignore_patterns = { 'target' },
+		mappings = {
+			i = global_mappings,
+			n = global_mappings,
+		},
+	},
 }
