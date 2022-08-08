2536f15b90b7485e02906687b79592b2f1cb9e87
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-08-08 15:53:59 +0200

    fix: TS formatting side effect

--- nvim/lua/lsp/langs/ts.lua
+++ nvim/lua/lsp/langs/ts.lua
@@ -4,7 +4,10 @@ local lsp_flags = config.lsp_flags
 local capabilities = config.capabilities
 
 require('lspconfig').tsserver.setup {
-	on_attach = on_attach,
+	on_attach = config.create_on_attach_effect(function(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end),
 	capabilities = capabilities,
 	settings = {},
 }
