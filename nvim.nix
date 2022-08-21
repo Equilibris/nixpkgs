d697afc2b121309e0208475563870b1fbd4d2a40
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-08-21 17:59:17 +0200

    fix: remove sumneko build, rather install with mason

--- nvim.nix
+++ nvim.nix
@@ -1,55 +1,8 @@
 { config, pkgs, lib, ... }:
-let
-  # https://github.com/NixOS/nixpkgs/blob/nixos-22.05/pkgs/development/tools/sumneko-lua-language-server/default.nix
-  luaLsp = pkgs.stdenv.mkDerivation {
-    name = "lua-lsp";
-    src = pkgs.fetchFromGitHub {
-      owner = "sumneko";
-      repo = "lua-language-server";
-      rev = "f407cb07ed559daf7a5a943d8896e849791ae5b7";
-      sha256 = "sha256-n54PWkiB+vXAqIOZ5FOTUNgGhAdBs81Q1WYxJ2XIb8o=";
-      fetchSubmodules = true;
-    };
-
-    nativeBuildInputs = [
-      pkgs.ninja
-      pkgs.makeWrapper
-    ];
-
-
-    preBuild = ''
-      cd 3rd/luamake
-    '';
-
-    postBuild = ''
-      cd ../..
-      ./3rd/luamake/luamake rebuild
-    '';
-
-    ninjaFlags = [
-      "-fcompile/ninja/${lib.toLower "Linux"}.ninja"
-    ];
-
-    installPhase = ''
-      runHook preInstall
-      install -Dt "$out"/share/lua-language-server/bin bin/lua-language-server
-      install -m644 -t "$out"/share/lua-language-server/bin bin/*.*
-      install -m644 -t "$out"/share/lua-language-server {debugger,main}.lua
-      cp -r locale meta script "$out"/share/lua-language-server
-      # necessary for --version to work:
-      install -m644 -t "$out"/share/lua-language-server changelog.md
-      makeWrapper "$out"/share/lua-language-server/bin/lua-language-server \
-        $out/bin/lua-language-server \
-        --add-flags "-E $out/share/lua-language-server/main.lua \
-        --logpath='~/.cache/sumneko_lua/log' \
-        --metapath='~/.cache/sumneko_lua/meta'"
-      runHook postInstall
-    '';
-  };
-in {
+{
   home.packages = with pkgs; [
     neovim 
-    luaLsp rust-analyzer statix stylua 
+    rust-analyzer statix stylua 
     ripgrep proselint wget
   ];
 
