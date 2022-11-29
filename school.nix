83acb20653a82a88ad32c463adafe41c389a86e2
William Sørensen <47296141+Equilibris@users.noreply.github.com>
2022-11-30 00:08:17 +0100

    add spell-checker

--- school.nix
+++ school.nix
@@ -3,15 +3,19 @@
 
 let
   school = true;
+
+  unstable = import <nixos-unstable> {};
 in
   if school then {
     home.packages = with pkgs; [
       teams openscad wxmaxima
 
+      hunspell unstable.hunspellDicts.nb-no unstable.hunspellDicts.en-gb-ize
+
       (texlive.combine {
         inherit (texlive)
           scheme-medium
-          runcode norewrites
+          runcode morewrites
           xifthen ifmtarg framed paralist
           titlesec
           blindtext;
