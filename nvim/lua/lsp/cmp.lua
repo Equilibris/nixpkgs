d96485244daff2d5b5dba02e845367aa1bc57914
William Sørensen <williambilly42@gmail.com>
2023-01-03 09:49:52 +0100

    readd tabnine

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -3,7 +3,8 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0
-		and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+		and vim.api
+				.nvim_buf_get_lines(0, line - 1, line, true)[1]
 				:sub(col, col)
 				:match '%s'
 			== nil
@@ -74,7 +75,7 @@ cmp.setup {
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' }, -- For luasnip users.
 		{ name = 'luasnip_choice' },
-		-- { name = 'cmp_tabnine' },
+		{ name = 'cmp_tabnine' },
 		-- { name = 'vsnip' }, -- For vsnip users.
 		--
 		-- { name = 'ultisnips' }, -- For ultisnips users.
