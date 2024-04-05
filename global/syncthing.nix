b41e9b038d5ded341d277f1c4b03d4993e6f31ab
William Sørensen <williambilly42@gmail.com.com>
2024-04-05 23:35:21 +0200

    misc

--- global/syncthing.nix
+++ global/syncthing.nix
@@ -20,7 +20,15 @@
       folders = {
         "nixpkgs" = {
           path = "/home/williams/.config/nixpkgs";
-          devices = [ "desktop-nixos" "surface-nixos" ];
+          devices = [ "desktop-nixos" "surface-nixos" "legion-2-nixos" ];
+        };
+        "dev" = {
+          path = "/home/williams/Dev";
+          devices = [ "desktop-nixos" "surface-nixos" "legion-2-nixos" ];
+        };
+        "obsidian" = {
+          path = "/home/williams/obsidian-vault";
+          devices = [ "desktop-nixos" "surface-nixos" "legion-2-nixos" ];
         };
       };
     };
