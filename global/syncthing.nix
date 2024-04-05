33d98510352d05ec5771892636a1ed0a59fe40bd
William Sørensen <ws423@cam.ac.uk>
2024-04-05 22:26:41 +0200

    add surface

--- global/syncthing.nix
+++ global/syncthing.nix
@@ -12,6 +12,7 @@
     settings = {
       devices = {
         "desktop-nixos" = { id = "7CXLHNK-TRKPUAB-PG2LR75-LN7OKYM-4OUDPJK-X7OUFEX-XGYU7KM-74RBKQ2"; };
+        "surface-nixos" = { id = "YWUSM63-CYUUMAN-QQYTOW6-MPS7TDC-MIF5DRZ-XYBRVMU-2JGTUCY-7RWTVQM"; };
         "iphone-xr-3"   = { id = "6CBBNBB-X4VC7GO-PNUKI32-B7EBALB-FOG42MG-N7HVZHS-JLB2EZN-HO3T7QS"; };
       };
       # folders = {
