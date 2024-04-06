b80d9bc080a8c1704e03dab9a2630a9ecbf4cb1f
William Sørensen <williambilly42@gmail.com.com>
2024-04-06 16:34:51 +0200

    add surface

--- global/syncthing.nix
+++ global/syncthing.nix
@@ -13,25 +13,26 @@
     settings = {
       devices = {
         "desktop-nixos" = { id = "X2H23T6-BNUWUVM-4DDJCUV-YUFUMEK-ZR6QHVR-IEVQVQY-JR3ODWH-CPDWNQN"; };
+        "surface-nixos" = { id = "S5Y7MO7-XVDGA7G-BVPMCIB-72KV3YJ-F3JCFK2-ASSHXSF-6QVRKZC-E6PKDAW"; };
       };
       folders = {
         "nixpkgs" = {
           path = "/home/williams/.config/nixpkgs";
           devices = [
-            "desktop-nixos" # "surface-nixos" "legion-2-nixos"
+            "desktop-nixos" "surface-nixos" # "legion-2-nixos"
           ];
         };
         "dev" = {
           path = "/home/williams/Dev";
           devices = [
-            "desktop-nixos" # "surface-nixos" "legion-2-nixos"
+            "desktop-nixos" "surface-nixos" # "legion-2-nixos"
           ];
         };
         "obsidian" = {
           path = "/home/williams/obsidian-vault";
           devices = [
             "desktop-nixos"
-            # "surface-nixos"
+            "surface-nixos"
             # "legion-2-nixos"
             # "iphone-xr-3"
           ];
